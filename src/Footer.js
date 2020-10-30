3300bf96f6cdf63b4d0b6f821a946ca5822a9222
lauracabtay <45257292+lauracabtay@users.noreply.github.com>
2020-10-30 21:10:17 +0000

    Create Footer.js component

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <p className="code-link">
+      <a href="https://github.com/lauracabtay/my-weather-app">
+        Open-source code&nbsp;
+      </a>
+      by Laura Cabantous
+    </p>
+  );
+}
