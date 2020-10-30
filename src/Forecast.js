c6ab27865f0d9cb49ed9d4c4febd84825656afb1
lauracabtay <45257292+lauracabtay@users.noreply.github.com>
2020-10-30 21:09:35 +0000

    Create Forecast.js component

---
+++ src/Forecast.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Forecast.css";
+
+export default function Forecast() {
+  return (
+    <div className="Forecast card c-carousel">
+      <div className="card-body cb-carousel">
+        <div className="row row-cols-1 row-cols-md-5"></div>
+      </div>
+    </div>
+  );
+}
