a0df4cce4b974b7e224235ad7301cd171efca4c5
lauracabtay <45257292+lauracabtay@users.noreply.github.com>
2020-10-31 09:18:20 +0000

    Install Bootstrap

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <p className="code-link">
-      <a href="https://github.com/lauracabtay/react-weather-app" target="_blank">
+      <a href="https://github.com/lauracabtay/react-weather-app" target="_blank" rel="noreferrer">
         Open-source code&nbsp;
       </a>
       by Laura Cabantous
