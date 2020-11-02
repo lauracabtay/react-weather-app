a92832966bf5e047bf46229fdffc92a8d710fafe
lauracabtay <45257292+lauracabtay@users.noreply.github.com>
2020-11-02 21:04:27 +0000

    Create Search Engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
-import FormatDate from "./FormatDate";
+import WeatherInfo from "./WeatherInfo";
 
-export default function Weather() {
+export default function Weather(props) {
+  const [weather, setWeather] = useState({ loaded: false});
+  const [city, setCity] = useState(props.defaultCity);
+
 function showWeather(response) {
     setWeather({
-      ready: true,
+      loaded: true,
       city: response.data.name,
       date: new Date(response.data.dt *1000),
       temperature: response.data.main.temp,
@@ -16,10 +19,6 @@ function showWeather(response) {
       icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     });
   }
-
-  let [weather, setWeather] = useState({ loaded: false});
-  let [city, setCity] = useState("");
-
 
   let form = (
     <form onSubmit={handleSubmit}>
@@ -53,77 +52,34 @@ function showWeather(response) {
       </div>
     </form>
   )
-
-  function updateInput(event) {
+  
+  function handleSubmit(event) {
     event.preventDefault();
+    search();
+  }
+  
+  function updateInput(event) {
     let updateInput = event.target.value;
     setCity(updateInput);
   }
+  
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    let apiKey = "528c21b30b50eb31aa5276a8d38b3d22";
-    let unit = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
-    axios.get(apiUrl).then(showWeather);
-  }
+          function search() {
+          const apiKey = "528c21b30b50eb31aa5276a8d38b3d22";
+          let unit = "metric";
+          let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+          axios.get(apiUrl).then(showWeather);
+            }
 
-if (weather.ready) {
+if (weather.loaded) {
   return (
     <div className="Weather">
       {form}
-       <div className="Currentweather row current-weather">
-         <div className="col-5">
-        <h1>{weather.city}</h1>
-        <h2 className="current-day">
-          <FormatDate currentDate={weather.date} /></h2>
-        <h2 className="current-weather">{weather.description}</h2>
-      </div>
-      <div className="col-1 current-weather">
-        <img
-          src={weather.icon}
-          alt={weather.description}
-          className="weather-icon"
-        />
-      </div>
-      <div className="col-2 current-info">
-        <p className="temperature">{Math.round(weather.temperature)}</p>
-        <div
-          className="btn-group degree-button"
-          role="group"
-          aria-label="Basic example"
-        >
-          <button type="button" className="btn btn-primary button-c">
-            °C
-          </button>
-          <button type="button" className="btn btn-dark button-f">
-            °F
-          </button>
-        </div>
-      </div>
-      <div className="col-3">
-        <div className="row-humidity">
-          <i className="fas fa-cloud-rain img-humidity img-humidity-large"></i>
-          <span>
-            <p className="p-humidity-large">{weather.humidity}%</p>
-          </span>
-        </div>
-        <div className="row-wind">
-          <i className="fas fa-wind img-wind img-wind-large"></i>
-          <span>
-            <p className="p-wind-large">{Math.round(weather.wind)} mph</p>
-          </span>
-        </div>
-      </div>
-    </div>
+       <WeatherInfo data={weather}/>
     </div>
   );
 } else {
-  return (
-    <div>
-    {form}
-    Loading...
-    </div>
-  )
+    search();
+    return "Loading...";
 }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,51 @@
+import React from "react";
+import FormatDate from "./FormatDate";
+
+
+export default function WeatherInfo(props) {
+   return (<div className="WeatherInfo Currentweather row current-weather">
+         <div className="col-5">
+        <h1>{props.data.city}</h1>
+        <h2 className="current-day">
+          <FormatDate currentDate={props.data.date} /></h2>
+        <h2 className="current-weather">{props.data.description}</h2>
+      </div>
+      <div className="col-1 current-weather">
+        <img
+          src={props.data.icon}
+          alt={props.data.description}
+          className="weather-icon"
+        />
+      </div>
+      <div className="col-2 current-info">
+        <p className="temperature">{Math.round(props.data.temperature)}</p>
+        <div
+          className="btn-group degree-button"
+          role="group"
+          aria-label="Basic example"
+        >
+          <button type="button" className="btn btn-primary button-c">
+            °C
+          </button>
+          <button type="button" className="btn btn-dark button-f">
+            °F
+          </button>
+        </div>
+      </div>
+      <div className="col-3">
+        <div className="row-humidity">
+          <i className="fas fa-cloud-rain img-humidity img-humidity-large"></i>
+          <span>
+            <p className="p-humidity-large">{props.data.humidity}%</p>
+          </span>
+        </div>
+        <div className="row-wind">
+          <i className="fas fa-wind img-wind img-wind-large"></i>
+          <span>
+            <p className="p-wind-large">{Math.round(props.data.wind)} mph</p>
+          </span>
+        </div>
+      </div>
+    </div>
+   )
+}
